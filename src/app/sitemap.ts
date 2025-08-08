ac9c21bd6ddfc0dc6fec0edae7773551045361ba
Deepaksharmadk <deepakdbg@gmail.com>
2025-08-08 14:03:56 +0530

    add index

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,5 +9,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "weekly",
             priority: 1.0,
         },
+        {
+            url: "https://dhurconvert.vercel.app/sanjay-cyber-center-patania",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.9,
+        },
     ];
 }
