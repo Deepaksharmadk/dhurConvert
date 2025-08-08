6d2a3781bd5a18231fa2ad9bd9bb6ae0d948a7b3
Deepaksharmadk <deepakdbg@gmail.com>
2025-08-08 14:35:50 +0530

    change cursor

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
         <div className='grid gap-4 m-4 sm:grid-cols-1 '>
             <div>
                 <footer>
-                    <Button c="black" justify="center" fullWidth color="yellow"> © 2025 Dhur Converter App. All rights reserved.<Text visibleFrom="md">
+                    <Button style={{ cursor: "not-allowed" }} c="black" justify="center" fullWidth color="yellow"> © 2025 Dhur Converter App. All rights reserved.<Text visibleFrom="md">
                         Developed by Deepak Sharma.9525468293</Text></Button>
                 </footer>
             </div>
